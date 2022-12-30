1ad54daf8aaabc74743d6569a121a7b7a57ca403
장은송 <janggoon235@gmail.com>
2022-12-30 13:42:17 +0900

    auto layout and stack view

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by silver j on 2022/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
