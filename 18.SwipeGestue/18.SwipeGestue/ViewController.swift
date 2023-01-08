8c746cdf5c6653a7955ad653c9de9657f04743a2
장은송 <janggoon235@gmail.com>
2023-01-08 22:25:05 +0900

    middle of swipe

--- 18.SwipeGestue/18.SwipeGestue/ViewController.swift
+++ 18.SwipeGestue/18.SwipeGestue/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imgViewUp: UIImageView!
+    @IBOutlet var imgViewLeft: UIImageView!
+    @IBOutlet var imgViewRight: UIImageView!
+    @IBOutlet var imgViewDown: UIImageView!
+    
+    var imgLeft = [UIImage]()
+    var imgRight = [UIImage]()
+    var imgUp = [UIImage]()
+    var imgDown = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
 
