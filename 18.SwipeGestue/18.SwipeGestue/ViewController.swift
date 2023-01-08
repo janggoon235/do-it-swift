062c1260c0d018b7cb6850b8911d5c4590314de8
장은송 <janggoon235@gmail.com>
2023-01-08 15:36:48 +0900

    swipe first commit

---
+++ 18.SwipeGestue/18.SwipeGestue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  18.SwipeGestue
+//
+//  Created by silver j on 2023/01/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
