f9c3b69ea0662af9ae45f45ef5f58ec5e85f960b
장은송 <janggoon235@gmail.com>
2023-01-10 13:00:39 +0900

    pinch gesture first commit

---
+++ 19.PinchGesture/19.PinchGesture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  19.PinchGesture
+//
+//  Created by silver j on 2023/01/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
