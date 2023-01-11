d9ac6a7e18b29d4ebf64869ed9954244213b7837
장은송 <janggoon235@gmail.com>
2023-01-11 21:24:56 +0900

    add basic pinch action

--- 19.PinchGesture/19.PinchGesture/ViewController.swift
+++ 19.PinchGesture/19.PinchGesture/ViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var txtPinch: UILabel!
+    
+    var initialFontsize:CGFloat!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.doPinch(_:)))
+        self.view.addGestureRecognizer(pinch)
+    }
+    
+    @objc func doPinch(_ pinch: UIPinchGestureRecognizer) {
+        if (pinch.state == UIGestureRecognizer.State.began) {
+            initialFontsize = txtPinch.font.pointSize
+        } else {
+            txtPinch.font = txtPinch.font.withSize(initialFontsize*pinch.scale)
+        }
     }
 
 
